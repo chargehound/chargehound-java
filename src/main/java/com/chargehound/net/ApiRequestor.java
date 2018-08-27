a523e584e81298f7df966c81af076d1c4cdf9367
jj <jj.weber@gmail.com>
2018-08-27 16:58:17 -0700

    Initial CH classes

---
+++ src/main/java/com/chargehound/net/ApiRequestor.java
@@ -0,0 +1,78 @@
+package com.chargehound.resources;
+
+import com.chargehound.Chargehound;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpHeaders;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.JsonObjectParser;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Map;
+
+class ApiRequestor {
+  private Chargehound chargehound;
+
+  // TODO: get configured version
+  public static final String CHARGEHOUND_USERAGENT = "Chargehound/v1 JavaBindings/";
+
+  static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+  static final JsonFactory JSON_FACTORY = new JacksonFactory();
+
+  public ApiRequestor (Chargehound chargehound) {
+    this.chargehound = chargehound;
+  }
+
+  public static String getParamsString(Map<String, String> params) throws UnsupportedEncodingException {
+    StringBuilder result = new StringBuilder();
+
+    for (Map.Entry<String, String> entry : params.entrySet()) {
+      result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+      result.append("=");
+      result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+      result.append("&");
+    }
+
+    String resultString = result.toString();
+    return resultString.length() > 0 ? resultString.substring(0, resultString.length() - 1) : resultString;
+  }
+
+  private String getUrl (String path, Map<String, String> params) {
+    try {
+      String query = this.getParamsString(params);
+      if (query.length() > 0) {
+        query = "?" + query;
+      }
+      return this.chargehound.getApiBase() + path + query;
+    } catch (UnsupportedEncodingException e) {
+      return this.chargehound.getApiBase() + path;
+    }
+  }
+
+  public HttpResponse request (String method, String path, Map<String, String> params, Map<String, String> data) throws IOException {
+    HttpRequestFactory requestFactory =
+        HTTP_TRANSPORT.createRequestFactory(new HttpRequestInitializer() {
+            @Override
+          public void initialize(HttpRequest request) {
+            HttpHeaders headers = request.getHeaders();
+            headers.setContentType("application/json");
+            headers.setUserAgent(CHARGEHOUND_USERAGENT);
+            headers.set("chargehound-version", "TODO: get the version here");
+
+            request.setParser(new JsonObjectParser(JSON_FACTORY));
+          }
+        });
+    String url = this.getUrl(path, params);
+    HttpRequest request = requestFactory.buildGetRequest(new GenericUrl(url));
+    HttpResponse response = request.execute();
+
+    return response;
+  }
+}
---
+++ src/main/java/com/chargehound/resources/Disputes.java
@@ -0,0 +1,20 @@
+package com.chargehound.resources;
+
+import com.chargehound.Chargehound;
+
+public class Disputes {
+  private Chargehound client;
+
+  // Creates a new disputes resource
+  public Disputes(Chargehound chargehound) {
+    this.client = client;
+  }
+
+  /**
+   * Retrieve a dispute
+   * This endpoint will return a single dispute.
+   */
+  public String retrieve (String id) {
+    return id;
+  }
+}
